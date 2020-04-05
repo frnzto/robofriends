223e59d14056574c053877808368fcbec12f01d8
frnzto <frnzto@gmail.com>
2020-04-05 11:20:36 +0300

    Updates

---
+++ precache-manifest.d8ac40563d26cf3c0c25c5283310139b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b97238e5f53f4fb0ccc67c59affbfb8d",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0eceb5b66c76922fdc47",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "7471e2699217902bf264",
+    "url": "/robofriends/static/css/main.4948b4d4.chunk.css"
+  },
+  {
+    "revision": "0eceb5b66c76922fdc47",
+    "url": "/robofriends/static/js/2.0144aeaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0144aeaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7471e2699217902bf264",
+    "url": "/robofriends/static/js/main.49c0869f.chunk.js"
+  },
+  {
+    "revision": "46690a7b9ddc777b3b4c",
+    "url": "/robofriends/static/js/runtime-main.ba4515c0.js"
+  }
+]);
